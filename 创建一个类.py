728ac085a625186105327ed0f31a84d960eb9b3e
飞菌 <251629543@qq.com>
2018-09-01 19:41:33 +0800

    test

--- 创建一个类.py
+++ 创建一个类.py
@@ -2,9 +2,8 @@ class Ab ():
     name = 'Sapurate'
     age = 18
     def hehe(self):
-        global age
-        age1 = age+1
-        print('虚岁为{0}'.format(age1))
+        age = a.age + 1
+        print('虚岁为{0}'.format(age))
         return None
 
 a = Ab()
